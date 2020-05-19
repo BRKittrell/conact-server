6f4fe9ee69090c1b727f979a29db93ba035c9156
Adam Ross <adamgarrettross@gmail.com>
2020-05-19 10:45:20 -0500

    Adding validation to post route in users

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/Users");
+const { check, validationResult } = require("express-validator");
 
 // Register a user route
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name").isLength({ min: 5 }),
+    check("email").isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    res.send("working just fine");
+  }
+);
 
 // Login a user
-router.get("/", (req, res) => {
-  res.send("Log a user in");
-});
+// router.get("/", (req, res) => {
+//   res.send("Log a user in");
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDb = require("./config/db");
+app.use(express.json({ extended: false }));
+connectDb();
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
